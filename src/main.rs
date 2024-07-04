44534cd449f9ca4f371aff494444561f3766acdf
Thymath <fj1994bc@gmail.com>
2024-07-04 16:20:54 +0200

    The empty window are now created.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod window;
+use window::App;
+
 fn main() {
     let instance_descriptor = wgpu::InstanceDescriptor {
         backends: wgpu::Backends::all(),
@@ -8,4 +11,19 @@ fn main() {
     for adapter in instance.enumerate_adapters(wgpu::Backends::all()) {
         println!("{:?}", adapter.get_info());
     }
+
+    let event_loop = winit::event_loop::EventLoop::new().unwrap();
+
+    // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
+    // dispatched any events. This is ideal for games and similar applications.
+    event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
+
+    // ControlFlow::Wait pauses the event loop if no events are available to process.
+    // This is ideal for non-game applications that only update in response to user
+    // input, and uses significantly less power/CPU time than ControlFlow::Poll.
+    event_loop.set_control_flow(winit::event_loop::ControlFlow::Wait);
+
+    let mut app = App::default();
+
+    let _ = event_loop.run_app(&mut app);
 }
