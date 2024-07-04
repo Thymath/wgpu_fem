1e55f5078e0cf03d198349d78ed088c9e33a266d
Thymath <fj1994bc@gmail.com>
2024-07-04 15:56:19 +0200

    Added message with the different adapters that wgpu can use.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let instance_descriptor = wgpu::InstanceDescriptor {
+        backends: wgpu::Backends::all(),
+        ..Default::default()
+    };
+    let instance = wgpu::Instance::new(instance_descriptor);
+
+    for adapter in instance.enumerate_adapters(wgpu::Backends::all()) {
+        println!("{:?}", adapter.get_info());
+    }
 }
